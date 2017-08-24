a4cb5e141c2fb7d1e099539da1e87ca5dfb2975c
marek331 <marek.tooming@treasuryview.com>
2017-08-24 11:59:59 +0300

    Added table storage connection strings. Added packages needed. Updated packages.

---
+++ AzureBeyondBasics/Controllers/StorageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureBeyondBasics.Controllers
+{
+    public class StorageController : Controller
+    {
+        // GET: Storage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    public class CustomerEntity : TableEntity
+    {
+        public CustomerEntity(Guid employeeId)
+        {
+            PartitionKey = "customer";
+            RowKey = employeeId.ToString();
+        }
+
+        public CustomerEntity() { }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
